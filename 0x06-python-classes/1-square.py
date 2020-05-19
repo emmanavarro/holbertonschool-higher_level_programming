0b8c9ed6696d257b22864f45a46b708ad55ab646
Emma Navarro <elnavarro55@gmail.com>
2020-05-19 22:07:38 +0000

    Fix documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-# Class Square that defines a square
+""" Creates a class Square that defines a square """
 
 
 class Square:
+    """ Square class """
     def __init__(self, size):
+        """__init__ is a constructor of square class
+        Args:
+            size (int): size of square
+        """
         self.__size = size

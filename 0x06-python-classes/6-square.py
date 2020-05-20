3c2173651a3e506aeb8d0103dcabe5d5356de8a4
Emma Navarro <elnavarro55@gmail.com>
2020-05-20 01:22:28 +0000

    Add 6-square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Creates a class Square that defines a square """
+
+
+class Square:
+    """ Square class with one privete atribute """
+    def __init__(self, size=0, position=(0, 0)):
+        """__init__ is a constructor of square class
+        Args:
+            size (int): size of square
+            position (tuple): position of Square
+        """
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """ Get method
+        return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        """ Get method
+        return: position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not isinstance(value[0], int) or
+                not isinstance(value[1], int) or
+                value[0] < 0 or
+                value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """ Calculates the area of the current Square
+        return: area
+        """
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """ print a square
+        """
+        if self.__size:
+            for i in range(self.__position[1]):
+                print()
+            for j in range(self.__size):
+                print("{}{}".format(" " * self.position[0], "#" * self.__size))
+        else:
+            print()

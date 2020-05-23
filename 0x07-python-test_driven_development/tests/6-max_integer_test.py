5105910145b0dd28d69d0506e4b378decc0e72b1
Emma Navarro <elnavarro55@gmail.com>
2020-05-23 05:17:41 +0000

    fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Unittest for max_integer
 """
-import unittest
+    Unittest
+"""
 
+import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """unittests for the function def max_integer(list=[])"""
-
-    def test_with_integers(self):
-        """tests with integer values"""
+    """
+        Testing function
+    """
+    def test_max_integer(self):
         self.assertEqual(max_integer([1, 2, 3]), 3)
         self.assertEqual(max_integer([-5, -30, -15, -23]), -5)
         self.assertEqual(max_integer([3.4, -2.1]), 3.4)
-        self.assertEqual(max_integer([]), None)
-        self.assertEqual(max_integer([float('inf'), 3]), float('inf'))
         self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([89]), 89)
         self.assertEqual(max_integer([1.3, 23, 10, 90.1, 90.2]), 90.2)
-
-    def test_with_strings(self):
-        """tests with strings values"""
-        self.assertEqual(max_integer("holberton"), 't')
-        self.assertEqual(max_integer("ABabcCzZ234"), 'z')
-        self.assertEqual(max_integer(['a', 'b', 'c', 'd']), 'd')
-
-if __name__ == '__main__':
-    unittest.main()

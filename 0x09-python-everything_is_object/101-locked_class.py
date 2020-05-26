4d80877f7f33a4d63c291e1f81cdce114c915327
Emma Navarro <elnavarro55@gmail.com>
2020-05-26 23:49:52 +0000

    Fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""class LockedClass with no
+class or object attribute
+"""
+
+
 class LockedClass:
-    __slots__ = 'first_name'
+    """Class that prevents different instance"""
+    __slots__ = ['first_name']

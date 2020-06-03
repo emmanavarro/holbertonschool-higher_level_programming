e10a05d7049971d1b3262ce4cfcce835e03a48b5
Emma Navarro <elnavarro55@gmail.com>
2020-06-03 22:06:40 +0000

    fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,4 +10,5 @@ def read_file(filename=""):
     """
 
     with open('my_file_0.txt', mode='r', encoding='utf-8') as my_file:
-        print(my_file.read(), end='')
+        line = my_file.read()
+        print(line, end='')

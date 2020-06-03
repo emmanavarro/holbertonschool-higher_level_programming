e8b7810d992a998bfe8b905db4bd0dde4b3e74b9
Emma Navarro <elnavarro55@gmail.com>
2020-06-03 22:19:29 +0000

    Fix

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-'''Read n lines module'''
+"""In this module it create a function that read
+    n lines of a file
+"""
 
 
 def read_lines(filename="", nb_lines=0):
-    '''function that reads n lines of a
+    """function that reads n lines of a
     text file and prints it to stdout
-    '''
+    """
 
     with open(filename, encoding='utf-8') as my_file:
 

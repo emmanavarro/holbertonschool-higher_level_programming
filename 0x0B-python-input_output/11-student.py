e9cde876408b281c68be097089c78229f7b0199a
Emma Navarro <elnavarro55@gmail.com>
2020-06-04 02:14:56 +0000

    Add 11-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Student to JSON module"""
+
+
+class Student:
+    """Class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name,
+        last_name and age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Method that retrieves a dictionary
+        representation of a Student instance
+        """
+
+        return self.__dict__

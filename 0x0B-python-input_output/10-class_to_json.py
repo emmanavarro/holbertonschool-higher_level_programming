5be43f5f20947b44886eb5b97a3d8c6d9041db51
Emma Navarro <elnavarro55@gmail.com>
2020-06-04 02:02:43 +0000

    add 10-class_to_json

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class to JSON module"""
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary
+    description with simple data structure
+    for JSON serialization of an object
+    """
+
+    return obj.__dict__

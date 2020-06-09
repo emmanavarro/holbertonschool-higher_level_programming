b447b78170f8546051540dcb92c8f3147d79003d
Emma Navarro <elnavarro55@gmail.com>
2020-06-09 19:44:17 +0000

    Add more tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -17,20 +17,61 @@ class BaseTests(unittest.TestCase):
 
     def test00(self):
         """Test number 0 for base"""
-        base0 = Base()
-        base1 = Base()
-        self.assertEqual(base0.id, 1)
-        self.assertEqual(base1.id, 2)
+        b_0 = Base()
+        b_1 = Base()
+        self.assertEqual(b_0.id, 1)
+        self.assertEqual(b_1.id, 2)
 
     def test01(self):
         """Test number 1 for base"""
-        base0 = Base("text")
-        self.assertEqual(base0.id, "text")
+        b_0 = Base("text")
+        self.assertEqual(b_0.id, "text")
 
     def test02(self):
-        """Test number 02 for base"""
-        b = Base.to_json_string(None)
-        self.assertEqual(b, "[]")
+        """Test number 2 for base"""
+        b_0 = Base(24)
+        self.assertEqual(b_0.id, 24)
+
+    def test03(self):
+        """Test number 3 for base"""
+        b_0 = Base("test")
+        self.assertEqual(b_0.id, "test")
+
+    def test04(self):
+        """Test number 4 for base"""
+        b_0 = Base(-57)
+        self.assertEqual(b_0.id, -57)
+
+    def test05(self):
+        """Test number 5 for base."""
+        b_0 = Base(None)
+        self.assertEqual(b_0.id, 1)
+
+    def test06(self):
+        """Test number 6 for base"""
+        b_0 = Base({"test": "test"})
+        self.assertEqual(b_0.id, {"test": "test"})
+
+    def test07(self):
+        """Test number 7 for base"""
+        b_0 = Base([1, 2, 3])
+        self.assertEqual(b_0.id, [1, 2, 3])
+
+    def test08(self):
+        """Test number 8 for base"""
+        b_0 = Base(27.8)
+        self.assertEqual(b_0.id, 27.8)
+
+    def test09(self):
+        """Test number 9 for base"""
+        b_0 = Base()
+        self.assertEqual(str(type(b_0)), "<class 'models.base.Base'>")
+        self.assertEqual(b_0.__dict__, {"id": 1})
+
+    def test10(self):
+        """Test number 10 for base"""
+        rect = Base.to_json_string(None)
+        self.assertEqual(rect, "[]")
 
 
 if __name__ == '__main__':

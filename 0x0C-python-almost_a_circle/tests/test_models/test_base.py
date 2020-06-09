31be43f3a273723a5b228c28191596b2678c1841
Emma Navarro <elnavarro55@gmail.com>
2020-06-09 19:51:45 +0000

    Add doc & pep8

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,7 +5,40 @@ Unittest for Base class
 
 
 import unittest
+import pep8
+import json
+import sys
+import io
+from models import base
 from models.base import Base
+
+
+class Test_Doc_Base(unittest.TestCase):
+    """ check for documentation """
+    def test_module_doc(self):
+        """ check for module documentation """
+        self.assertTrue(len(base.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ check for documentation """
+        self.assertTrue(len(Base.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ check for method documentation """
+        for func in dir(Base):
+            self.assertTrue(len(func.__doc__) > 0)
+
+
+class Test_Pep8_Base(unittest.TestCase):
+    """ check for pep8 validation """
+    def test_pep8(self):
+        """ test base and test_base for pep8 conformance """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/base.py'
+        file2 = 'tests/test_models/test_base.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
 
 
 class BaseTests(unittest.TestCase):
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,7 +5,37 @@ Unittest for Square
 
 
 import unittest
+import pep8
 from models.square import Square
+from models import square
+
+
+class Test_Doc_Square(unittest.TestCase):
+    """ checking for documentation """
+    def test_module_doc(self):
+        """ checking for module documentation """
+        self.assertTrue(len(square.__doc__) > 0)
+
+    def test_class_doc(self):
+        """ checking for class documentation """
+        self.assertTrue(len(Square.__doc__) > 0)
+
+    def test_method_docs(self):
+        """ checking for method documentation """
+        for func in dir(Square):
+            self.assertTrue(len(func.__doc__) > 0)
+
+
+class Test_Pep8_Square(unittest.TestCase):
+    """ checking for pep8 validation """
+    def test_pep8(self):
+        """ testing square and test_square for pep8 """
+        style = pep8.StyleGuide(quiet=True)
+        file1 = 'models/square.py'
+        file2 = 'tests/test_models/test_square.py'
+        result = style.check_files([file1, file2])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warning).")
 
 
 class SquareTests(unittest.TestCase):

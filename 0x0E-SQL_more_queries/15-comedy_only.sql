1f567408a03083c9bd63ca3c2f9ae2c1e869c0d5
Emma Navarro <elnavarro55@gmail.com>
2020-07-15 02:43:56 +0000

    Fix genre_id colum for tsg line 3

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,3 @@
 -- Lists all Comedy shows in the database hbtn_0d_tvshows
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
-SELECT ts.title FROM tv_shows ts INNER JOIN tv_show_genres tsg ON tsg.show_id = ts.id INNER JOIN tv_genres tg ON tg.id = tgs.genre_id WHERE tg.name = 'Comedy' GROUP BY ts.title ORDER BY ts.title ASC;
+SELECT ts.title FROM tv_shows ts INNER JOIN tv_show_genres tsg ON tsg.show_id = ts.id INNER JOIN tv_genres tg ON tg.id = tsg.genre_id WHERE tg.name = 'Comedy' GROUP BY ts.title ORDER BY ts.title ASC;

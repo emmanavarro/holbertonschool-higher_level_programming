77afef295d59a5808c7c9491385b9e114758b9a1
Emma Navarro <elnavarro55@gmail.com>
2020-08-18 23:52:59 +0000

    Find a peak in list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" sends a request to a URL passed as an argument, and displays only the status code of the response """
+""" sends a request to a URL passed as an argument,
+    and displays only the status code of the response """
+
 
 def find_peak(list_of_integers):
     """Find peak in list"""

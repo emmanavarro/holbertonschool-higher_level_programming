72f2ca6b0b9e86c3fbce79dbc653d99329c5d6c3
Emma Navarro <elnavarro55@gmail.com>
2020-08-18 23:46:58 +0000

    Temporal algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,16 @@
 """ sends a request to a URL passed as an argument, and displays only the status code of the response """
 
 def find_peak(list_of_integers):
-    if len(list_of_integers) is 0:
-        return None
-    return max(list_of_integers)
+    """Find peak in list"""
+    listt = list_of_integers.copy()
+    for idx in range(len(listt)):
+        if (idx - 1) >= 0 and (idx + 1) <= (len(listt) - 1):
+            if listt[idx] >= listt[idx - 1] and listt[idx] >= listt[idx + 1]:
+                return listt[idx]
+        if (idx - 1) < 0:
+            if listt[idx] >= listt[idx + 1]:
+                return listt[idx]
+        if (idx + 1) > (len(listt) - 1):
+            if listt[idx] >= listt[idx - 1]:
+                return listt[idx]
+    return None

5e8b6e15811627343f54ea263b328e024932169c
emmanavarro <elnavarro55@gmail.com>
2020-09-15 16:15:24 -0500

    Change argument passed to console.log

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -4,7 +4,7 @@ Script that prints the first argument passed to it
 */
 const args = process.argv;
 if (args[2]) {
-  console.log('Holberton');
+  console.log(args[2]);
 } else {
   console.log('No argument');
 }

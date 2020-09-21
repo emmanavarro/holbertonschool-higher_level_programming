03cb52a91df20a23d84b6b383ffd1db837e913f3
emmanavarro <elnavarro55@gmail.com>
2020-09-21 18:57:41 -0500

    script to print a square where class Square inherits

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Square1 = require('./5-square.js');
+
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let letter = 0; letter < this.height; letter++) {
+        console.log('C'.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;

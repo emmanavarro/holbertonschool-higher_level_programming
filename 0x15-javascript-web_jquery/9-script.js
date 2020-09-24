49bfdab429e4cb5dff2f99f4398cf80095e30bb7
emmanavarro <elnavarro55@gmail.com>
2020-09-24 17:45:24 -0500

    Fetches and displays the value of a key in an URL

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,6 @@
+// Fetches from https://fourtonfish.com/hellosalut/?lang=fr and
+// displays the value of hello from that fetch in the HTML’s tag DIV#hello
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+$.get(url, function (data, textStatus) {
+  $('DIV#hello').text(data.hello);
+});
